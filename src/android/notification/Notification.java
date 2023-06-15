13b2f78c3e197c24218e5ba1b8e6b54a5d570a1f
Austin Hunter <ahunter8850@outlook.com>
2023-06-15 10:11:26 -0500

    Update Notification.java

--- src/android/notification/Notification.java
+++ src/android/notification/Notification.java
@@ -55,6 +55,7 @@ import androidx.core.app.NotificationCompat;
 import static android.app.AlarmManager.RTC;
 import static android.app.AlarmManager.RTC_WAKEUP;
 import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
+import static android.app.PendingIntent.FLAG_CANCEL_CURRENT;
 import static android.os.Build.VERSION.SDK_INT;
 import static android.os.Build.VERSION_CODES.M;
 import static androidx.core.app.NotificationCompat.PRIORITY_HIGH;
